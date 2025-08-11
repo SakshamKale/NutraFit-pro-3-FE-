554f86f8353c550dfa2116cc5c2bf4edeef6f1d7
saksham kale <sakshamkale4056@gmail.com>
2025-08-11 11:20:02 +0530

    Add Pro-3 folder files

--- src/App.js
+++ src/App.js
@@ -7,19 +7,10 @@
 
 import CommonForm from "./CommonForm";
 import CheckBox from "./component/CheckBox";
-import Cc from "./Pro-3/Cc";
-import FridaySchedule from "./Pro-3/FridaySchedule";
-import HomePage from "./Pro-3/HomePage";
+
+
 import Default from "./Pro-3/Main";
-import MondaySchedule from "./Pro-3/MondaySchedule";
-import MonMenu from "./Pro-3/MonMenu";
-import NutraFitChat from "./Pro-3/NutraFitChat";
-import SaturdaySchedule from "./Pro-3/SaturdaySchedule";
-import ThursdaySchedule from "./Pro-3/ThursdaySchedule";
-import TueMenu from "./Pro-3/TueMenu";
-import TuesdaySchedule from "./Pro-3/TuesdaySchedule";
-import WedMenu from "./Pro-3/WedMenu";
-import WednesdaySchedule from "./Pro-3/WednesdaySchedule";
+
 import GlobalNotification from "./Pro-3/GlobalNotification";
 
 // import MainPage from "./Pro-3/MainPage";
